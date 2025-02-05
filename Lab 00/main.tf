6156990b59032f7752adb636fd61839bb3627cdb
Yoichi Nagumo <kelpin.workspace@gmail.com>
2025-02-05 14:02:16 +0700

    Hi !

--- Lab 00/main.tf
+++ Lab 00/main.tf
@@ -1,28 +1,22 @@
 locals {
-  # Valid Google Cloud Storage Classes
   valid_classes = ["STANDARD", "NEARLINE", "COLDLINE", "ARCHIVE"]
-  # Error Message
   error_message = "Error ! Only Receive Standard, Nearline, Coldline, Archive"
 }
-
 
 resource "google_storage_bucket" "storage_bucket" {
   # Create Google Storage Bucket
   name = var.storage_name
-  # On Region
   location = var.region
-  # With Storage Class
   storage_class = var.storage_class
 
   # Lifecycle : Control Terraform Handle Resources Creation, Update, Deletions
+  # Prevent Accidental Deletion
   lifecycle {
-    # Prevent Accidental Deletion
     prevent_destroy = true
     
     # Precondition to Enforce Conditions Before Applying to Resources
     precondition {
       condition = contains(local.valid_classes, var.storage_class)
-      # Return Error if Storage Classes Other Than the Defined
       error_message = local.error_message
     }
   }
